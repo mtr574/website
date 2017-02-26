e315a8cc476b0c039f920f0fd3ac7c5042729b9b
--unset <nate@konimbo.co.il>
2017-02-26 16:18:21 +0200

    npm script to copy views files

--- npm_scripts/copy.js
+++ npm_scripts/copy.js
@@ -2,8 +2,13 @@
 var fs = require('fs'),
     wd = process.cwd(),
     appFolder = wd + '/app/',
+    viewsFolder = wd + '/app/views',
     publicFolder = wd + '/public/',
     publicFileName = ['404.html', 'index.html', 'app.js', 'config.js', 'routes.js', 'app.css'];
+// copy files
 for (var i = 0; i < publicFileName.length; i++) {
     fs.createReadStream(appFolder + publicFileName[i]).pipe(fs.createWriteStream(publicFolder + publicFileName[i]));
 }
+
+// copy views
+// TODO: copy files from views folder into public
