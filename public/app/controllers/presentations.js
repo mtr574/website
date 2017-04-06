0164c1e6e59d2339b260b4c50c625d9ee546ab8b
natekonimbo <nate@konimbo.co.il>
2017-04-06 17:56:57 +0300

    errors with ngRoute

--- public/app/controllers/presentations.js
+++ public/app/controllers/presentations.js
@@ -2,7 +2,7 @@ app.controller('presentations', function($scope) {
     $scope.presentations = [{
         title: 'Slack architecture',
         subtitle: 'a great presentation by Keith Adams Chief Architect @ Slack',
-        tags: [architecture, with - slides],
+        tags: ['architecture'],
         link: 'https://www.infoq.com/presentations/slack-infrastructure?utm_source=infoq&utm_medium=slideshare&utm_campaign=slidesharesf'
     }]
 })
